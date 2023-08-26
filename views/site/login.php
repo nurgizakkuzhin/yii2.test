f5c845c132d0faab1432bb0d97f4a342782eda8b
Nurgiz <akkuzhin.n@gmail.com>
2023-08-26 14:44:30 +0300

    Авторизация

--- views/site/login.php
+++ views/site/login.php
@@ -11,13 +11,13 @@ use yii\bootstrap5\Html;
 $this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-login">
+<div class="site-login" style="margin-left: 30px">
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>Please fill out the following fields to login:</p>
 
     <div class="row">
-        <div class="col-lg-5">
+        <div class="col-lg-4">
 
             <?php $form = ActiveForm::begin([
                 'id' => 'login-form',
